098225d3474e0fbee840d77e619be3c59dcb03bc
Jose Agostinho Renner <agostinhorenner@hotmail.com>
2026-05-10 20:51:34 -0300

    upd readme + remove services folder

--- services/unidades_conservacao.js
+++
@@ -1,7 +0,0 @@
-// import { UnidadeConservacao } from "../models/index.js";
-
-// export async function getUnidadeConservacao(id) {
-//   let data = await UnidadeConservacao.findByPk(id);
-
-//   return data;
-// }
