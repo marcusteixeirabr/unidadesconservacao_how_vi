be3c4b1bac002e9d039a3ac2f613f8e9018cbc64
Jose Agostinho Renner <agostinhorenner@hotmail.com>
2026-05-11 17:56:04 -0300

    getAllComunicacao

--- routes/comunicados.js
+++ routes/comunicados.js
@@ -4,6 +4,16 @@ export const comunicadosRouter = Router();
 import { mysqlConnection } from "../config/database.js";
 import Comunicacao from "../models/comunicacao.js";
 
+comunicadosRouter.get("/", async (req, res) => {
+  try {
+    await new Comunicacao(mysqlConnection).getAllComunicacao();
+    res.status(201).end();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Ocorreu um erro durante o processamento, tente novamente");
+  }
+});
+
 comunicadosRouter.post("/", async (req, res) => {
   try {
     await new Comunicacao(mysqlConnection).createComunicacao(req.body);
